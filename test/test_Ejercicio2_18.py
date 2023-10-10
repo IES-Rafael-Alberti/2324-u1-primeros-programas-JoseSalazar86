655937eba111cfb0c4b638e09fa2be0b7cc81af9
JoseSalazar86 <jasalazar@rafaelalberti.es>
2023-10-10 16:37:50 +0200

    modified:   test/test_Ejercicio2_18.py

--- test/test_Ejercicio2_18.py
+++ test/test_Ejercicio2_18.py
@@ -2,4 +2,7 @@ from src.Ejercicio2_18 import nombreCompleto
 
 def test_nombreCompleto():
 
-    pass
+    resultado = nombreCompleto("John Doe")
+    assert(resultado[0], "john doe")  
+    assert(resultado[1], "JOHN DOE")  
+    assert(resultado[2], "John Doe")
